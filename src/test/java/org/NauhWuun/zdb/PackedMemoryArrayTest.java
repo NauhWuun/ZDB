ed042b38e479c417201334c4a35cd2d06c8789e6
NauhWuun <NauhWuun2009@gmail.com>
2020-07-25 00:33:33 +0800

    Update PackedMemoryArrayTest.java

--- src/test/java/org/NauhWuun/zdb/PackedMemoryArrayTest.java
+++ src/test/java/org/NauhWuun/zdb/PackedMemoryArrayTest.java
@@ -14,7 +14,7 @@ public class PackedMemoryArrayTest
 		PackedMemoryArray<Integer, Integer> arr = new PackedMemoryArray<>(100);
 
 		arr.add(1, 1, 10);
-		arr.add(2,22, 30);
+		arr.add(2, 22, 30);
 
 		System.out.println(arr.get(1).getKey() + "," + arr.get(1).getValue());
 		System.out.println(arr.get(2).getKey() + "," + arr.get(2).getValue());
